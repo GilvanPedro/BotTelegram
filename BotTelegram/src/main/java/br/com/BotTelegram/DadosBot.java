4af4a1d00fb7048cbdca646b378ff768079f7926
Gilvan Pedro <87739497+GilvanPedro@users.noreply.github.com>
2025-12-10 16:27:03 -0300

    Update DadosBot.java

--- BotTelegram/src/main/java/br/com/BotTelegram/DadosBot.java
+++ BotTelegram/src/main/java/br/com/BotTelegram/DadosBot.java
@@ -1,6 +1,6 @@
 package br.com.BotTelegram;
 
 public class DadosBot {
-    public static final String BOT_TOKEN ="8397720970:AAG_b4HQhW_xz5dYc1JEbh2M4gw21gDFBv0";
-    public static final String BOT_USER_NAME = "gilvannp_bot";
+    public static final String BOT_TOKEN ="SEU_TOKEN";
+    public static final String BOT_USER_NAME = "NOME_DO_BOT";
 }
